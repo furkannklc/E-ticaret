9e8c5b10e5e670e08b1edcf507f9df939d4da53b
furkannklc <furkannkilic43@gmail.com>
2025-07-16 14:34:38 +0300

    route yapısı ile id verisini alma

--- src/app/components/detail-component/detail-component.ts
+++ src/app/components/detail-component/detail-component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-detail-component',
@@ -6,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './detail-component.html',
   styleUrl: './detail-component.css'
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
+  urunId!: number ;
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const idParam = params.get('id');
+      if (idParam !== null) {
+        this.urunId = +idParam;
+        console.log("Ürün ID:", this.urunId);
+      }
+    })
+    }
 
 }
